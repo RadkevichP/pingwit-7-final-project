7e858aca90896ccbc2cdabcbc05062cb0746be4a
Pavel Radkevich <pradkevich@eisgroup.com>
2024-04-06 12:57:01 +0300

    Initial commit. Lecture 45

---
+++ src/main/java/pl/pingwit/pingwitcarrental/PingwitcarrentalApplication.java
@@ -0,0 +1,13 @@
+package pl.pingwit.pingwitcarrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingwitcarrentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingwitcarrentalApplication.class, args);
+	}
+
+}
